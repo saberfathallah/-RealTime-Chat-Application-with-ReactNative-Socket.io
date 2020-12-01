c8031273afbabe81c24f38482de7a8eb0d325ee8
saber <saberfathallah7@gmail.com>
2020-12-01 12:48:12 +0100

    getAll msgs

--- src/screens/Messages/Messages.js
+++ src/screens/Messages/Messages.js
@@ -1,11 +1,43 @@
-import React from "react";
-import { Text } from "react-native";
+import React, { useContext } from "react";
+import { View, Text } from "react-native";
+import { ListItem, Avatar } from "react-native-elements";
+import { UserContext } from "@/contexts";
 
-const Friends = () => (
-    <>
-    <Text style={{ fontSize: 12, color: "#FF0D10" }}>
-    Messages
-  </Text>
-  </>
-);
-export default Friends;
+import { CONVERSATION } from "../../constants/routes";
+
+const Messages = ({ navigation }) => {
+  const {
+    state: { conversations },
+  } = useContext(UserContext);
+  console.log(
+    "conversationsconversationsconversationsconversations",
+    conversations
+  );
+  return (
+    <View>
+      {conversations.map(({ user, conversation }) => (
+        <ListItem key={user.id} bottomDivider>
+          <Avatar
+            onPress={() =>
+              navigation.navigate(CONVERSATION, { userId: user.id })
+            }
+            source={{
+              uri:
+                "https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg",
+            }}
+          />
+          <ListItem.Content>
+            <ListItem.Title>
+              {user.firstName} {user.lastName}
+            </ListItem.Title>
+            <ListItem.Subtitle>{user.email}</ListItem.Subtitle>
+          </ListItem.Content>
+          <Text style={{ fontSize: 12, color: "#FF0D10" }}>
+            {conversation[conversation.length - 1].text}
+          </Text>
+        </ListItem>
+      ))}
+    </View>
+  );
+};
+export default Messages;
